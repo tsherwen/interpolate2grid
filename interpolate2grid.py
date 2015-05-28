6a6c933f628bf47d0896c4f18055cc23927e785a
ts551 <ts551@york.ac.uk>
2015-05-28 18:08:22 +0100

    updated mulitple day reader

--- interpolate2grid.py
+++ interpolate2grid.py
@@ -7,15 +7,28 @@ from interpolate2grid_tools import *
 # -----------------------------------------------------------------------------
 # ----------  Driver ----------
 def main( case='chlorophyll', nc_data=None, nc_lat=None,\
-        nc_lon=None, csv_lon=None, csv_lat=None,\
+        nc_lon=None, csv_lon=None, csv_lat=None, csv_time=None,\
         multidate=False, debug=True):
     """ 
+    This program calls functions to extract data for a given cruise track 
+    from a 2D (lon, lat) array. 
+
+    Data is flagged if it is below 0. 
+        
+    Direct extraction of data, interpolated values, and locations are then  
+    saved to CSV ( with dates included in the names if multiple dates
+    provided )
+
+    -------- README --------
     Setup output here - change variables names to columns headers 
     within given files
     
     Set "debug" to True to output a test plot and information on interpolation.
     
     Set "multidate" to True to output for multiple days
+    
+    Set case to be data input (to access stored varible names)
+        e.g. 'sea ice' or 'chlorophyll'
     
     Variables are:
     nc_data : Name of data variable in NetCDF file
@@ -29,17 +42,17 @@ def main( case='chlorophyll', nc_data=None, nc_lat=None,\
     arr_file, locs_file = get_input_files()
     
     # Retrieve variables for data files  (Unless these are given)
-    vars = nc_data, nc_lat, nc_lon, csv_lon, csv_lat
+    vars = nc_data, nc_lat, nc_lon, csv_lon, csv_lat, csv_time
     if any( [ isinstance( i, type(None) ) for i in vars ] ):
         vars = var_store(case)
-        nc_data, nc_lat, nc_lon, csv_lon, csv_lat = vars
+        nc_data, nc_lat, nc_lon, csv_lon, csv_lat, csv_time = vars
     if debug:
-        print vars, nc_data, nc_lat, nc_lon, csv_lon, csv_lat
+        print vars
 
     # Extract and give output data on a day by day basis ( if mulitdate==True) 
     if multidate:
         pass
-        get_dates
+        dates = get_obs_dates( locs_file, csv_time=csv_time) 
     else:
         # Only process for the single date
         dates = [None]  
@@ -49,19 +62,20 @@ def main( case='chlorophyll', nc_data=None, nc_lat=None,\
 
         # Get locations of cruise from CSV
         locs = extract_CSV( locs_file, date=date, csv_lon=csv_lon, \
-                csv_lat=csv_lat )
+                csv_lat=csv_lat, csv_time=csv_time,debug=debug )
 
         # Get NetCDF data to extract from
         data, lon, lat = extract_NetCDF(arr_file, date=date,\
                 nc_data=nc_data, format=format,\
-                nc_lat=nc_lat, nc_lon=nc_lon)
+                nc_lat=nc_lat, nc_lon=nc_lon, debug=debug)
 
-        print [  
-        [i.min(), i.max(), i.mean(), i.std() ] for i in [np.ma.array(data)] 
-        ]
+        if debug:
+            print '>'*10, [   [i.min(), i.max(), i.mean(), i.std() ] for i in\
+                [np.ma.array(ii) for ii in lat, lon, data, locs ]  ]
 
         # Interpolate, save to csv, and return values
-        vals = extract_nearest_point_from_arr( data, lon, lat, locs )
+        vals = extract_nearest_point_from_arr( data, lon, lat, locs,  \
+            date=date, debug=debug )
 
         # Plot up test plot
         if debug:
@@ -70,4 +84,13 @@ def main( case='chlorophyll', nc_data=None, nc_lat=None,\
 # -----------------------------------------------------------------------------
 # -----------------------------------------------------------------------------
 # call to main/driver...
-main()
+#debug=True
+debug=False
+
+# Setup for multidates/Rosie's data ( unhash to set )
+multidate=True
+case='sea ice'
+main(multidate=multidate, case=case, debug=debug)
+
+# Setup for single dates/Steve's data ( unhash to set )
+#main()
--- interpolate2grid_tools.py
+++ interpolate2grid_tools.py
@@ -8,6 +8,12 @@ from scipy.interpolate import griddata
 import matplotlib.pyplot as plt
 import scipy.stats as stats
 
+# time processing
+import datetime
+from datetime import datetime as datetime_
+import dateutil
+import time
+from time import mktime
 
 # -----------------------------------------------------------------------------
 # -----------------------------------------------------------------------------
@@ -26,7 +32,7 @@ def get_input_files(debug=False):
 
 def extract_NetCDF( arr_file, date=None, format='classic',\
             nc_lat='G3fakeDim0', nc_data='l3m_data',\
-            nc_lon='G3fakeDim1', debug=False):
+            nc_lon='G3fakeDim1', nc_time='time', debug=False):
     """ 
     Extract and standardise input data to array form from given NetCDF 
     and CSV files 
@@ -43,38 +49,98 @@ def extract_NetCDF( arr_file, date=None, format='classic',\
             lat =  f.variables[nc_lat]
             data = f.variables[nc_data]
         else:
-#            f =f[ ]
             lon= f.variables[nc_lon]
             lat =  f.variables[nc_lat]
-            data = f.variables[nc_data]
-            
+            data = f.variables[nc_data] 
+            dates = f.variables[nc_time]
+                        
+            # convert (NetCDF) time to datetime
+            ANSI = time.strptime(dates.units[11:], '%Y-%m-%d %H:%M:%S')
+            ANSI = datetime.datetime( *ANSI[:6] )
+            dates = [ add_days( ANSI, i ) for i in dates ]
+            dates = np.array([
+            datetime.datetime(*i.timetuple()[:3]) for i in dates
+            ] )
+            # Select given date 
+            if debug:
+                print data.shape
+            data = data[dates==date,...][0,...]
+            if debug:
+                print [ i.shape for i in data, lon, lat ]
+
+            # Kludge - Lon mislabel?
+#            tmp = np.array( lat )
+#            lat = np.array( lon )
+#            lon = lat
+
         lon, lat, data = [ np.array(i) for i in  lon, lat, data ]
+    debug=True
     if debug:
         print [ [type(i), i.shape] for i in lon, lat, data ]
     return data.T, lon, lat
 
-
-
 def extract_CSV( locs_file, csv_lon='LONG', csv_lat='LAT',  \
-            date=None, debug=False):
+            csv_time=None, date=None, debug=False):
+    """
+        Extract Location lat and lons & conv. to DataFrame (LON, LAT)
+        
+        If date is given, only return data for this date
+    """
+    df =pd.read_csv( locs_file )
+    if isinstance(date, type(None)):
+        locs =  pd.DataFrame( [df[csv_lon], df[csv_lat] ] ).values
+    else:
+        # retrieve dates and only consdier daily resolution
+        dates =  df[csv_time].apply(dateutil.parser.parse)
+        dates = [datetime.datetime(*i.timetuple()[:3]) for i in dates] 
+        if debug:
+            print [type(i) for i in df[csv_lon], df[csv_lat], dates ]
+        locs =  pd.DataFrame( {csv_lon:df[csv_lon].values, 
+                        csv_lat:df[csv_lat].values, csv_time:dates} )    
+        if debug:
+            print 'all locs:',  locs
+        locs = locs[locs[csv_time]==date]
+        locs = np.array( [ locs[csv_lon].values, locs[csv_lat].values] )
+        if debug:
+            print 'locs for given date: {}'.format( date ), locs
+        
+    del df
+    return locs
+
+def get_obs_dates( locs_file, csv_time='yyyy-mm-ddThh24:mi[GMT/UT]' ,\
+            debug=False):
     """
         Extract Location lat and lons & conv. to DataFrame (LON, LAT)
     """
     df =pd.read_csv( locs_file )
-    locs =  pd.DataFrame( [df[csv_lon], df[csv_lat] ]  )
+    dates =  df[csv_time].apply(dateutil.parser.parse)    
     del df
-    return locs.values
+    # get unique list of days
+    dates = sorted( set( [ 
+    datetime.datetime(*i.timetuple()[:3]) for i in dates 
+    ] ))
 
-def interpolate_locs2grid( data, lon, lat, locs  ):
+    return dates
+
+def interpolate_locs2grid( data, lon, lat, locs, debug=False ):
     """ 
         Interpolate over given array (data) for locations (locs) at Lon. 
         and Lat. , returning values 
     """
     # --- Interpolate over given array
     lo,la = np.meshgrid(lon, lat)
-    print type( data )
-    vals = griddata((lo.ravel(),la.ravel()) , data.ravel(), locs.T, method='linear' )# [0,:], locs[1,:] )
-    return vals    
+    if debug:
+        print '>'*5, type( data ), locs.shape
+    vals = griddata((lo.ravel(),la.ravel()) , data.ravel(),    \
+                        locs.T, method='linear' )# [0,:], locs[1,:] )
+    return vals
+
+def add_days(sourcedate,days_):
+    """
+        Incremental increase datetime by given days
+    """
+    sourcedate += datetime.timedelta(days=days_)
+    return sourcedate
 
 def nan_helper(y):
     """
@@ -108,55 +174,82 @@ def var_store( case='sea ice'):
         nc_lon='G3fakeDim1'
         csv_lon='LONG'
         csv_lat='LAT'
+        csv_time=None
 #        format='classic'
 
     if case == 1:
-        nc_data='l3m_data'
-        nc_lat='G3fakeDim0'
-        nc_lon='G3fakeDim1'
+        nc_data='Sea_Ice_Concentration_with_Final_Version'
+        nc_lat='latitude'
+        nc_lon='longitude'
 #        csv_lon='seatex-gga-lon (degrees)'
-#        csv_timestamp='Timestamp'
+#        csv_time='Timestamp'
 #        csv_lat=' seatex-gga-lat (degrees)'
         csv_lat='Latitude[deg+veN]'
         csv_lon='Longitude[deg+veE]'
-#        csv_timestamp='yyyy-mm-ddThh24:mi[GMT/UT]'
+        csv_time='yyyy-mm-ddThh24:mi[GMT/UT]'
 #        format='classic'
 
-    return nc_data, nc_lat, nc_lon, csv_lon, csv_lat
+    return nc_data, nc_lat, nc_lon, csv_lon, csv_lat, csv_time
 
-def test_spatial_plot( data, lon, lat, locs, vals ):
+def test_spatial_plot( data, lon, lat, locs, vals, cmap=plt.cm.Greens ):
     """
         Plot up a surface of grided data given with extracted values overlaid
     """
     fig = plt.figure(figsize=(20,15), dpi=80, facecolor='w', edgecolor='k')
-#    vmin, vmax = data.min(), data.max()
-    vmin, vmax = 0, data.max()
-    cmap=plt.cm.Greens
+    vmin, vmax = data.min(), data.max()
+    #    vmin, vmax = 0, data.max()
     s_z = [ 
     cmap(  (float(i) - vmin) / ( np.array([vmin,vmax]) ).ptp() )  \
     for i in vals ]
-    print vmin, vmax
+#    s_z = np.array( s_z )
+    print vmin, vmax, s_z
     plt.pcolor( lon, lat, data.T, cmap=plt.cm.Blues, vmin=vmin, vmax=vmax )
-    plt.scatter( locs[0,:], locs[1,:] , cmap=plt.cm.Greens, c=vals, \
+    plt.scatter( locs[0,:], locs[1,:], cmap=plt.cm.Greens, c=s_z, \
                             vmin=vmin, marker='x', vmax=vmax  )    
     plt.show()
 
 
-def extract_nearest_point_from_arr( data, lon, lat, locs, debug=False ):
+def extract_nearest_point_from_arr( data, lon, lat, locs, date=None, \
+            output_filename='./interpolated_values', debug=False ):
     """
         Interpolate data to grid to account for missing data.
         
-        Save to CSV
-
+        Data is flagged if it is below 0. 
+        
+        Direct extraction of data, interpolated values, and locations are then  
+        saved to CSV ( with dates included in the names if multiple dates
+        provided )
 
     """
+    # Is location within provided grid?
+    out_lon = [ n for n, i in  enumerate( locs[0,:] ) if 
+            ( ( i<np.min(lon) ) or (i>np.max(lon) ) ) ]
+    out_lat =  [ n for n, i in  enumerate( locs[1,:] ) if 
+                ( ( i<np.min(lat) ) or (i>np.max(lat) ) ) ]
+    indexs =sorted( set(out_lon+out_lat ) )
+    
+    print '!'*100, 'REMOVED: CRUISE POINT(s) OUT OF PROVIDED GRID WITH  \
+LAT+LON: {} '.format( [ list(locs[:,i]) for i in indexs] )
+    if debug:
+        print [ (i, type(i), i.shape) for i in [locs] ]
+    for index in indexs[::-1]:
+        locs = np.delete( locs, index, 1)
+    if debug:
+        print [ (i, type(i), i.shape) for i in [locs] ]
+    print locs
+
     # Interpolate over given array and create flag array for masked values
-    flags  = interpolate_locs2grid( data, lon, lat, locs  )
+    flags  = interpolate_locs2grid( data, lon, lat, locs )
+    raw_extract =flags
+    print flags
     flags[flags>0] = 0
     flags[ flags<0] =1
 
+    print flags
+    
     # Interpolate for masked values (those below zero) and provide values 
-    data[data<0]=np.nan
+    if len(data[data<0]) > 0:
+        data[data<0]=np.nan
     nans, x= nan_helper(data)
     data[nans]= np.interp(x(nans), x(~nans), data[~nans])
     if debug:
@@ -169,13 +262,20 @@ def extract_nearest_point_from_arr( data, lon, lat, locs, debug=False ):
     vals  = interpolate_locs2grid( data, lon, lat, locs  )
 
     # Print output to csv
-    output_file = './interpolated_values.csv'
+    if not isinstance( date, type(None) ): 
+        output_filename=output_filename+'{}_{}_{}'.format( date.year, \
+            date.month, date.day )
+    
     print vals.shape
     lo,la = np.meshgrid(lon, lat)
     print [i.shape for i in  flags, vals, la.ravel(), lo.ravel() ]
-    df = pd.DataFrame( {'Interp. Values': vals, 'Lon.':locs[0,:], 
-                'Lat.':locs[1,:], 'flag': flags } )
-    df.to_csv( output_file )
+    df = pd.DataFrame( {
+    'Interpolated values': vals, 
+    'Longitude':locs[0,:], 
+    'Latitude':locs[1,:], 
+    'Raw extracted values': raw_extract,
+    'Flag': flags, } )
+    df.to_csv( output_filename+'.csv' )
     return vals
 
 
